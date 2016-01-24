bdf8ff933094c35431c4e93fb03ed43c2ec078c9
Manpreet <manpreet.singh@salesforce.com>
2016-01-24 20:39:46 +0530

    added vector operations

--- Chapter_02/maths-for-ml/src/main/scala/Vector.scala
+++ Chapter_02/maths-for-ml/src/main/scala/Vector.scala
@@ -1,4 +1,5 @@
-import breeze.linalg.{DenseVector, SparseVector}
+import breeze.linalg._
+import breeze.stats.mean
 
 /**
   * Created by manpreet.singh on 23/01/16.
@@ -19,6 +20,49 @@ object Vector {
     val m:SparseVector[Double] = sv.mapActivePairs((i,x) => x+1)
     println(m)
 
+    // spark mllib support for local vector
+    // Create a dense vector (1.0, 0.0, 3.0).
+    //val dv1: Vector = Vectors.dense(1.0, 0.0, 3.0)
+    // Create a sparse vector (1.0, 0.0, 3.0) by specifying its indices and values corresponding to nonzero entries.
+    //val sv1: Vector = Vectors.sparse(3, Array(0, 2), Array(1.0, 3.0))
+    // Create a sparse vector (1.0, 0.0, 3.0) by specifying its nonzero entries.
+    //val sv2: Vector = Vectors.sparse(3, Seq((0, 1.0), (2, 3.0)))
+
+    // vector operations (mul, dot)
+    val a = DenseVector(0.56390, 0.36231, 0.14601, 0.60294, 0.14535)
+    val b = DenseVector(0.15951, 0.83671, 0.56002, 0.57797, 0.54450)
+    println(a.t * b)
+    println(a dot b)
+
+    // find mean
+    val mn = mean(DenseVector(0.0,1.0,2.0))
+    println(mn)
+
+    // normalize
+    val v = DenseVector(-0.4326, -1.6656, 0.1253, 0.2877, -1.1465)
+    val nm = norm(v, 1)
+    println(nm)
+
+    // min-max
+    val v1 = DenseVector(2, 0, 3, 2, -1)
+    println(argmin(v1))
+    println(argmax(v1))
+    println(min(v1))
+    println(max(v1))
+
+    // to array
+    val ar = DenseVector(1, 2, 3)
+    println(ar.toArray)
+    println(ar.data)
+
+    // compare ops
+    val a1 = DenseVector(1, 2, 3)
+    val b1 = DenseVector(1, 4, 1)
+    println((a1 :== b1))
+    println((a1 :<= b1))
+    println((a1 :>= b1))
+    println((a1 :< b1))
+    println((a1 :> b1))
   }
 
 }

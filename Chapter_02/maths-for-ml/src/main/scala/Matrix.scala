7e90f0de248ecf87ad99511d9cbf573c8532b942
Rajdeep Dua <dua_rajdeep@yahoo.com>
2016-01-24 10:26:40 -0800

    matrix operations

--- Chapter_02/maths-for-ml/src/main/scala/Matrix.scala
+++ Chapter_02/maths-for-ml/src/main/scala/Matrix.scala
@@ -1,19 +1,31 @@
 import breeze.linalg.{DenseMatrix, DenseVector}
 
 
-object Matrix {
+object MatrixOperations {
 
   def main(args: Array[String]) {
     val a = DenseMatrix((1,2),(3,4))
-    println("a : \n" + a)
-    val m = DenseMatrix.zeros[Int](5,5)
-    println("Created a 5x5 matrix\n" + m )
-    println( "m.rows :" + m.rows + " m.cols : "  + m.cols)
-    println("First Column of m : \n" + m(::,1))
+    val b = DenseMatrix((2,2),(2,2))
+    val c = a + b
+    println("a + b : \n" + c)
+    val d = a*b
+    println("Dot product a*b : \n" + d)
 
-    m(4,::) := DenseVector(5,5,5,5,5).t
-    println("Assigned 5,5,5,5,5 to last row of m.\n")
-    println(m)
+    val e = 	a :+= 1
+    val f = a :< b
+
+    println("Inplace Addition : \n" + e)
+
+    /*
+    Inplace addition	a :+= 1.0
+    Inplace elementwise multiplication	a :*= 2.0
+    Vector dot product	a dot b,a.t * b†	dot(a,b)	dot(a,b)
+    Elementwise sum	sum(a)
+    Elementwise max	a.max
+    Elementwise argmax	argmax(a)
+    Ceiling	ceil(a)
+    Floor	floor(a)
+    */
 
   }
 

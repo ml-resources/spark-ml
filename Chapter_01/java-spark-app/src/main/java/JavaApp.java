3cec5ba4f6ca47d3a58178789d2cb13145161a37
Manpreet <manpreet.singh@salesforce.com>
2016-01-28 21:17:00 +0530

    cleanup and common code added

--- Chapter_01/java-spark-app/src/main/java/JavaApp.java
+++ Chapter_01/java-spark-app/src/main/java/JavaApp.java
@@ -1,10 +1,6 @@
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
-
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -20,18 +16,6 @@ public class JavaApp {
 //           ####### create spark context by starting the cluster locally using 2 CPU cores #######
             sc = new JavaSparkContext("local[2]", "First Spark App");
 
-//           ####### we take the raw data in CSV format and convert it into a set of records of the form (user, product, price),
-//           (without lambda for < jdk 1.8) #######
-
-//            JavaRDD<String[]> data = sc.textFile("data/UserPurchaseHistory.csv")
-//                    .map(new Function<String, String[]>() {
-//                        @Override
-//                        public String[] call(String s) throws Exception {
-//                            return s.split(",");
-//                        }
-//                    });
-
-//           ####### with lambda #######
             JavaRDD<String[]> data = sc.textFile("data/UserPurchaseHistory.csv").map(s -> s.split(","));
 
 //           ####### let's count the number of purchases #######
@@ -39,52 +23,16 @@ public class JavaApp {
 
             System.out.println("NumberPurchases : " + numPurchases);
 
-//           ####### let's count how many unique users made purchases (without lambda for < jdk 1.8) #######
-//            long uniqueUsers = data.map(new Function<String[], String>() {
-//                @Override
-//                public String call(String[] strings) throws Exception {
-//                    return strings[0];
-//                }
-//            }).distinct().count();
-
-//           ####### with lambda #######
             long uniqueUsers = data.map(strings -> strings[0]).distinct().count();
 
+//           ####### let's count the number of users #######
             System.out.println("Unique users : " + uniqueUsers);
 
-//          ####### let's sum up our total revenue (without lambda for < jdk 1.8) #######
-//            Double totalRevenue = data.map(new Function<String[], Double>() {
-//                @Override
-//                public Double call(String[] strings) throws Exception {
-//                    return Double.parseDouble(strings[2]);
-//                }
-//            }).reduce(new Function2<Double, Double, Double>() {
-//                private static final long serialVersionUID = 1L;
-//
-//                public Double call(Double v1, Double v2) throws Exception {
-//                    return new Double(v1.doubleValue() + v2.doubleValue());
-//                }
-//            });
-
-//           ####### with lambda #######
             Double totalRevenue = data.map(strings -> Double.parseDouble(strings[2])).reduce((Double v1, Double v2) -> new Double(v1.doubleValue() + v2.doubleValue()));
 
+//           ####### let's count the total revenue #######
             System.out.println("Total revenue : " + totalRevenue);
 
-//          ####### let's find our most popular product (without lambda for < jdk 1.8) #######
-//            List<Tuple2<String, Integer>> pairs = data.mapToPair(new PairFunction<String[], String, Integer>() {
-//                @Override
-//                public Tuple2<String, Integer> call(String[] strings) throws Exception {
-//                    return new Tuple2(strings[1], 1);
-//                }
-//            }).reduceByKey(new Function2<Integer, Integer, Integer>() {
-//                @Override
-//                public Integer call(Integer integer, Integer integer2) throws Exception {
-//                    return integer + integer2;
-//                }
-//            }).collect();
-
-//           ####### with lambda #######
             List<Tuple2<String, Integer>> pairs = data.mapToPair(strings -> new Tuple2<String, Integer>(strings[1], 1)).reduceByKey((Integer i1, Integer i2) -> i1 + i2).collect();
 
             Map<String, Integer> sortedData = new HashMap<>();
--- Chapter_02/maths-for-ml/src/main/scala/linalg/operations/common.scala
+++ Chapter_02/maths-for-ml/src/main/scala/linalg/operations/common.scala
@@ -1,6 +1,7 @@
 package linalg.operations
 
-import breeze.linalg.{DenseMatrix, DenseVector}
+import breeze.linalg._
+import breeze.numerics.{floor, ceil}
 
 /**
   * Created by manpreet.singh on 28/01/16.
@@ -15,35 +16,73 @@ object common {
   val m1 = DenseMatrix((2, 3, 1), (4, 5, 1), (6, 7, 1))
   val m2 = DenseMatrix((3, 4, 1), (2, 6, 1), (8, 2, 1))
 
-  // add operation
+  // elementwise add operation
   def add(): Unit = {
     println(v1 + v2)
     println(m1 + m2)
   }
 
-  // mul operation
+  // elementwise mul operation
   def mul(): Unit = {
     println(v1 :* v2)
     println(m1 :* m2)
   }
 
-  // compare operation
+  // elementwise compare operation
   def compare(): Unit = {
     println(v1 :< v2)
     println(m1 :< m2)
   }
 
   // inplace addition operation
-  def inplace(): Unit = {
-    println(v1 :+= 2)
-    println(m1 :+= 3)
+//  def inplace(): Unit = {
+//    println(v1 += 2)
+//    println(m1 += 3)
+//  }
+
+  // vector dot product operation
+  def dot(): Unit = {
+    println(v1 dot v2)
   }
 
+  // elementwise sum
+  def sumof(): Unit = {
+    println(sum(v1))
+  }
+
+  // elementwise max
+  def maxElement(): Unit = {
+    println(max(v1))
+    println(max(m1))
+  }
+
+  // elementwise argmax
+  def argmaxElement(): Unit = {
+    println(argmax(v1))
+    println(argmax(m1))
+  }
+
+  // ceiling (Rounds numbers to the next highest integer)
+  def ceiling(): Unit = {
+    println(ceil(v1))
+    println(ceil(m1))
+  }
+
+  // floor (Rounds numbers to the next lowest integer.)
+  def flooring(): Unit = {
+    println(floor(v1))
+    println(floor(m1))
+  }
 
   def main(args: Array[String]) {
     add()
     mul()
     compare()
-    inplace()
+    dot()
+    sumof()
+    maxElement()
+    argmaxElement()
+    ceiling()
+    flooring()
   }
 }

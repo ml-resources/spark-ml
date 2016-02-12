fc065c23765f7a12369c0d05b12d4d8559d2e2e6
Rajdeep Dua <dua_rajdeep@yahoo.com>
2016-02-12 08:01:08 -0800

    changed basic vector example spark

--- Chapter_02/spark/src/main/scala/linalg/vector/SparkVector.scala
+++ Chapter_02/spark/src/main/scala/linalg/vector/SparkVector.scala
@@ -5,14 +5,15 @@ object SparkVector {
 
   def main(args: Array[String]): Unit = {
     // Create a dense vector (1.0, 0.0, 3.0).
+
     val dVectorOne: Vector = Vectors.dense(1.0, 0.0, 2.0)
     println("dVectorOne:" + dVectorOne)
-    // Create a sparse vector (1.0, 0.0, 3.0) by specifying its indices and values
+    //  Sparse vector (1.0, 0.0, 2.0, 3.0)
     // corresponding to nonzero entries.
-    val sVectorOne: Vector = Vectors.sparse(3, Array(0, 2), Array(1.0, 3.0))
-    // Create a sparse vector (1.0, 0.0, 3.0) by specifying its
+    val sVectorOne: Vector = Vectors.sparse(4,  Array(0, 2,3),  Array(1.0, 2.0, 3.0))
+    // Create a sparse vector (1.0, 0.0, 2.0, 2.0) by specifying its
     // nonzero entries.
-    val sVectorTwo: Vector = Vectors.sparse(3, Seq((0, 1.0), (2, 3.0)))
+    val sVectorTwo: Vector = Vectors.sparse(4,  Seq((0, 1.0), (2, 2.0), (3, 3.0)))
 
     println("sVectorOne:" + sVectorOne)
     println("sVectorTwo:" + sVectorTwo)
